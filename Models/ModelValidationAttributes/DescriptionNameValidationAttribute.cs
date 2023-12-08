5d980e17480c3d7145f30356de04916982cb7bf4
HlibBondarev <Hlib.Bondarev@gmail.com>
2023-12-08 11:57:28 +0200

    Added validation of products: changed Product model, added validation attribute - DescriptionNameValidation and DescriptionValidation, edited tags for Id and Type in view Create and Edit

---
+++ Models/ModelValidationAttributes/DescriptionNameValidationAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProductsValidation.Models.ModelValidationAttributes
+{
+	// TODO: + Create a validation attribute for the condition - Description should not be equal to Name but should start with the Name of the product
+	public class DescriptionNameValidationAttribute : ValidationAttribute
+	{
+		public DescriptionNameValidationAttribute()
+		{
+			ErrorMessage = "Description should not be equal to Name but should start with the Name of the product!";
+		}
+
+		public override bool IsValid(object? value)
+		{
+			if (value is not Product product)
+			{
+				ErrorMessage = "The parameter must be a product type!";
+				return false;
+			}
+			if (string.IsNullOrEmpty(product.Description)) return true;
+			if (product.Description.StartsWith(product.Name))
+			{
+				return product.Description != product.Name.Trim();
+			}
+			return false;
+		}
+	}
+}
